566cd1d4216fee7fe1fa60cb3bf305955ddab1e9
Yanick <tyankee15@gmail.com>
2020-08-17 13:40:07 -0300

    created featured speakers and main program sections

---
+++ scripts/navbar.js
@@ -0,0 +1,17 @@
+// /* only execute this script when the document is ready */
+// $(document).ready(function () {
+//     /* function called when you click of the button */
+//     $("button").click(function () {
+
+//         /* this if else to change the text in the button */
+//         if ($("button").text() == "☰") {
+//             $("button").text("🞬");
+//         } else {
+//             $("button").text("☰");
+//         }
+
+//         /* this function toggle the visibility of our "li" elements */
+//         $("li").toggle("slow");
+//     });
+// });
+
